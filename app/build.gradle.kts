8fe71d45f60789cc1b0442cbc260d76ace8596d3
mahmoudhindam9-stack <mahmoudhindam9@gmail.com>
2026-09-09 14:12:39 +0300

    build: bump version to 1.5
    Update versionCode to 6 and versionName to 1.5 for the next release.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -22,8 +22,8 @@ android {
     applicationId = "com.aistudio.musicplayer.abcde"
     minSdk = 24
     targetSdk = 36
-    versionCode = 5
-    versionName = "1.4"
+    versionCode = 6
+    versionName = "1.5"
     testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
   }
 
