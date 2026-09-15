b5110b55ef5821928a65ec20b0b491e15250e99e
mahmoudhindam9-stack <mahmoudhindam9@gmail.com>
2026-09-15 14:41:40 +0300

    chore: bump version to 3.5.2
    Increment version code and name for release 3.5.2.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -20,8 +20,8 @@ android {
     applicationId = "com.aistudio.musicplayer.abcde"
     minSdk = 24
     targetSdk = 36
-    versionCode = 28
-    versionName = "3.5.1" // Auto-release trigger
+    versionCode = 29
+    versionName = "3.5.2" // Auto-release trigger
     testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
   }
 
