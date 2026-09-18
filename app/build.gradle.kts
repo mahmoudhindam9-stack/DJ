3e67abf30d9aa984a4e341f90c63b31f479738d9
mahmoudhindam9-stack <mahmoudhindam9@gmail.com>
2026-09-18 12:12:15 +0300

    build: bump version to 3.6.3

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -20,8 +20,8 @@ android {
     applicationId = "com.aistudio.musicplayer.abcde"
     minSdk = 24
     targetSdk = 36
-    versionCode = 39
-    versionName = "3.6.2" // Auto-release trigger
+    versionCode = 40
+    versionName = "3.6.3" // Auto-release trigger
     testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
   }
 
