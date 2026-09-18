723f8da4b8a2897e3cb2c81a5882f4679acca0da
mahmoudhindam9-stack <mahmoudhindam9@gmail.com>
2026-09-18 10:18:30 +0300

    fix: add missing nav typography import

--- app/src/main/java/com/example/ui/components/BottomNavBar.kt
+++ app/src/main/java/com/example/ui/components/BottomNavBar.kt
@@ -13,6 +13,7 @@ import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.unit.sp
 import androidx.compose.ui.unit.dp
 
 @Composable
