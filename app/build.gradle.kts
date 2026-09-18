f3b55c4c8175efa6cc28a5b6777801667b13331f
mahmoudhindam9-stack <mahmoudhindam9@gmail.com>
2026-09-18 10:16:51 +0300

    chore: bump version to 3.5.5

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -20,8 +20,8 @@ android {
     applicationId = "com.aistudio.musicplayer.abcde"
     minSdk = 24
     targetSdk = 36
-    versionCode = 31
-    versionName = "3.5.4" // Auto-release trigger
+    versionCode = 32
+    versionName = "3.5.5" // Auto-release trigger
     testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
   }
 
